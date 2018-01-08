f7fcd3db90bb6b9dff6c0fc9f40dbe16621d51f7
עמיחי דניאל מנטנבנד <amichaiman@m.telhai.ac.il>
2018-01-08 14:19:11 +0200

    end of lab commit

---
+++ TestHashTable.java
@@ -0,0 +1,54 @@
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+
+public class TestHashTable {
+	
+	public static void main(String[] args) {
+		HashTable hash = new HashTable();
+		
+		InputStreamReader reader = new InputStreamReader(System.in);
+	
+		StreamTokenizer tokens = new StreamTokenizer(reader);
+		
+		try {
+			while (tokens.nextToken() == StreamTokenizer.TT_WORD){		//while next token is string
+				switch (tokens.sval){
+					case ("add"):{							
+						tokens.nextToken();
+						HashTableData newData = new HashTableData(tokens.sval);		
+						hash.insert(newData);
+						System.out.println("new element added biatch!");
+						} break;
+						
+					
+					case ("remove"):{								//gets value to matrix at i,j
+						tokens.nextToken();
+						HashTableData newData = new HashTableData(tokens.sval);		
+						hash.remove(newData);
+						System.out.println("element removed sis!");
+					} break;
+					
+					case ("clear"):
+						hash.clear();
+						System.out.println("nothing inside me anymore (2)");
+					; break;
+		
+					case ("print"):{
+						hash.showStructure();
+						System.out.println("structure showed!");
+					} break;
+					
+	
+					case ("Quit"):{								
+						return;
+					}
+					default:	
+						System.out.println("The command '" + tokens.sval + "' is invalid");
+				}
+			}
+		}
+		catch(Exception e){
+			System.out.println("exception catched motherfacakkkkka!");
+	}
+	}
+}
